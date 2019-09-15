fb1380c559e4772dad5cfa8043d0d4f816fdadd4
Amtomkraftwerk <antonthielmann@t-online.de>
2019-09-15 17:30:23 +0200

    added the nextplayer UI inside overall UI

--- R/2.4_overallUI.R
+++ R/2.4_overallUI.R
@@ -3,15 +3,25 @@
 #' @keywords internal jaipur_ui
 jaipur_ui <- fluidPage(shinyalert::useShinyalert(),
                        # Sidebar layout with input and output definitions
-                       sidebarLayout(
-                         # Sidebar panel for Market, Tokens and Camels
+                       sidebarLayout(# Sidebar panel for Market, Tokens and Camels
                          sidebarPanel(
                            # Call the defined sidebarUI
-                           sidebarUI('jaipur'),
+                           tagList(
+                             # Plot the Market on sidepanel
+                             plotOutput("marketplot", height = 280),
 
-                           # Actionbutton for restarting the Game
-                           shinyWidgets::actionBttn(inputId = "restart",
-                                                    label = "Restart")
+                             # Plot Tokens under the Market
+                             helpText("Tokens"),
+                             tableOutput("Tokens"),
+
+                             #Number of Cards left in the Deck on the bottom of sidepanel
+                             helpText("Deck"),
+                             verbatimTextOutput("Deck"),
+
+                             # Actionbutton for restarting the Game
+                             shinyWidgets::actionBttn(inputId = "restart",
+                                                      label = "Restart")
+                           )
                          ),
 
                          # MainPanel with Player's Panels
@@ -27,15 +37,46 @@ jaipur_ui <- fluidPage(shinyalert::useShinyalert(),
                                h4("Select goods from the market to swap or take"),
 
                                # Add predefined Player1_UI
-                               player1_UI('jaipur')
+                               player1_UI('jaipur'),
+                               # Output of player 1's hand
+                               plotOutput("plot_player_1",
+                                          height = 280,
+                                          width = 700),
+
+
+                               # Number of Players Camels under the Tokens
+                               helpText("Number of Camels - Player 1"),
+                               verbatimTextOutput("camels_player_1")
                              ),
 
                              # Intermediate Panel so Player's can't see others cards
-                             tabPanel("Next Player",
-                                      h4("Click to continue"),
+                             tabPanel(
+                               "Next Player",
+                               h4("Click to continue"),
 
-                                      # Add predefined nextPlayerUI
-                                      nextPlayerUI("Jaipur")),
+                               # Add predefined nextPlayerUI
+                               # Actionbutton to continue to next Panel
+                               actionButton("continue",
+                                            label = "Continue"),
+
+                               # Textoutput - gives last Player's action
+                               textOutput("opponents_action"),
+                               tags$head(
+                                 # Define text style with html
+                                 tags$style(
+                                   "#opponents_action{color: black;
+                                 font-size: 30px;
+                                 font-style: arvo;
+                                 }"
+                                 )
+                               ),
+
+                               # Plot the action -selling and taking- on nextPlayerPanel
+                               plotOutput("actionplot", height = 280),
+
+                               # Plot the action -swapping- on nextPlayerPanel
+                               plotOutput("swappingplot", height = 280)
+                             ),
 
                              # Panel of Player 1
                              tabPanel(
@@ -44,8 +85,19 @@ jaipur_ui <- fluidPage(shinyalert::useShinyalert(),
                                h4("Select goods from the market to swap or take"),
 
                                # Add predefined Player2_UI
-                               player2_UI('jaipur')
+                               player2_UI('jaipur'),
+
+
+                               # Output of player 2's hand
+                               plotOutput("plot_player_2",
+                                          height = 280,
+                                          width = 700),
+
+
+
+                               # Number of Players Camels under the Tokens
+                               helpText("Number of Camels - Player 2"),
+                               verbatimTextOutput("camels_player_2")
                              )
                            )
-                         )
-                       ))
+                         )))
