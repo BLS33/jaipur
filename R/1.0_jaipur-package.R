d560a463727c168d125907e8cc041c497ec9eb00
Amtomkraftwerk <antonthielmann@t-online.de>
2019-09-22 21:24:22 +0200

    Package description

---
+++ R/1.0_jaipur-package.R
@@ -0,0 +1,11 @@
+#' Use R for Playing the card Game Jaipur
+#'
+#' This is a R implementation of the card game Jaipur
+#'
+#' @name jaipur_package
+#' @aliases jaipur
+#' @docType package
+#' @author Anton Thielmann, Brandon Schwab
+#' @keywords package
+#' @examples ## jaipur:.start_jaipur("Player one", "Player two")
+NULL
