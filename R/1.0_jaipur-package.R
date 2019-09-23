49cc86653ea9368ad8f6836bd234fd80b5cadcb8
Amtomkraftwerk <antonthielmann@t-online.de>
2019-09-23 11:40:32 +0200

    documentation

--- R/1.0_jaipur-package.R
+++ R/1.0_jaipur-package.R
@@ -1,6 +1,7 @@
 #' Use R for Playing the card Game Jaipur
 #'
 #' This is the R implementation of the card game Jaipur.
+#' It is a two player game, but played on a single monitor.
 #'
 #' @name jaipur-package
 #' @docType package
